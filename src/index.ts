19dc4c85327b8f9c3c60e6efa0349255bf57efd5
Harish Puri <harishpuri.in@gmail.com>
2026-07-12 09:28:14 +0530

    setup

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-const gretting: string = 'Hello World';
-console.log(gretting);
+import express, { Request, Response, Express } from "express";
+import { config } from "dotenv";
+
+const app: Express = express();
+const PORT: number = Number(process.env.PORT) || 5000;
+
+config();
+
+app.use(express.json());
+
+app.use(express.urlencoded());
+
+app.listen(PORT, () => {
+  console.log(`Application is Running on ${PORT}: MODE: ${process.env.MODE}`);
+});
