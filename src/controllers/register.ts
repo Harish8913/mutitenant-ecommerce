3b4cae7f4503ff4e10b3380746d4ef31886c4f7d
Harish Puri <harishpuri.in@gmail.com>
2026-07-18 23:34:20 +0530

    first flow

--- src/controllers/register.ts
+++ src/controllers/register.ts
@@ -1,6 +1,52 @@
 import { Request, Response } from "express";
-import { prisma } from '../lib/prisma.js'
+import { prisma } from "../lib/prisma.js";
+import bcrypt from "bcrypt";
 
-const registerUser = async () => {
+type user = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  mobileNo: string;
+  countryCode: string;
+  roleId: number;
+};
 
-}
+export const registerUser = async (
+  req: Request<{}, {}, user>,
+  res: Response,
+) => {
+  try {
+    const { body } = req;
+    const existingUser = await prisma.user.findUnique({
+      where: { email: body.email },
+    });
+
+    // If User exists
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exist" });
+    }
+
+    // If User doesn't exists
+    const { password, ...userData } = body;
+    const hash = await bcrypt.hash(password, 10);
+    const newUser = await prisma.user.create({
+      data: { passwordHash: hash, ...userData },
+      select: {
+        userId: true,
+        firstName: true,
+        lastName: true,
+        email: true,
+        mobileNo: true,
+        countryCode: true,
+        roleId: true,
+      },
+    });
+
+    return res
+      .status(201)
+      .json({ message: "User registered successfully", user: newUser });
+  } catch (err) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, Express } from "express";
 import { config } from "dotenv";
+import authRouter from "./routes/auth.routes.js";
 
 const app: Express = express();
 const PORT: number = Number(process.env.PORT) || 5000;
@@ -9,6 +10,8 @@ config();
 app.use(express.json());
 
 app.use(express.urlencoded());
+
+app.use('/api', authRouter);
 
 app.listen(PORT, () => {
   console.log(`Application is Running on ${PORT}: MODE: ${process.env.MODE}`);
